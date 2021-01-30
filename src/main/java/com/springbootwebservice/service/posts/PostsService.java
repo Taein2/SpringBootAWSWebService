539457b0c2014dc722c98a7fca06586cbbf71e9c
Taein2 <dls102s@gmail.com>
2021-01-30 20:59:43 +0900

    chapter3

---
+++ src/main/java/com/springbootwebservice/service/posts/PostsService.java
@@ -0,0 +1,40 @@
+package com.springbootwebservice.service.posts;
+
+import com.springbootwebservice.domain.posts.Posts;
+import com.springbootwebservice.domain.posts.PostsRepository;
+import com.springbootwebservice.web.dto.PostsResponseDto;
+import com.springbootwebservice.web.dto.PostsSaveRequestDto;
+import com.springbootwebservice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(posts);
+    }
+}
