ec2e8b9854dc3bd1b0ea7370e50c9433a217bee4
Taein2 <dls102s@gmail.com>
2021-02-06 19:51:49 +0900

    chapter4

--- src/main/java/com/springbootwebservice/service/posts/PostsService.java
+++ src/main/java/com/springbootwebservice/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.springbootwebservice.service.posts;
 
 import com.springbootwebservice.domain.posts.Posts;
 import com.springbootwebservice.domain.posts.PostsRepository;
+import com.springbootwebservice.web.dto.PostsListResponseDto;
 import com.springbootwebservice.web.dto.PostsResponseDto;
 import com.springbootwebservice.web.dto.PostsSaveRequestDto;
 import com.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -38,6 +42,7 @@ public class PostsService {
         postsRepository.delete(posts);
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
@@ -45,4 +50,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
