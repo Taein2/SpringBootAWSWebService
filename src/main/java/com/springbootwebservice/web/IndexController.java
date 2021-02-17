79cf0b78eead94c8fb6ce13ebde47f32742e5a6b
Taein2 <dls102s@gmail.com>
2021-02-18 01:15:50 +0900

    chapter7 AWS RDS

--- src/main/java/com/springbootwebservice/web/IndexController.java
+++ src/main/java/com/springbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springbootwebservice.web;
 
+import com.springbootwebservice.config.auth.LoginUser;
 import com.springbootwebservice.config.auth.dto.SessionUser;
 import com.springbootwebservice.service.posts.PostsService;
 import com.springbootwebservice.web.dto.PostsResponseDto;
@@ -18,10 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
